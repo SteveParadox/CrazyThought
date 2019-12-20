467f0e964a322aa5c847210b7def47adca098208
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2019-12-20 02:44:25 +0100

    Add files via upload

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,18 +6,27 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import Config
 from flask_socketio import SocketIO
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from flask_paranoid import Paranoid
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.config.from_object(Config)
 
-db= SQLAlchemy()
-bcrypt= Bcrypt()
-login_manager= LoginManager()
-login_manager.login_view ='users.login'
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 socketio = SocketIO()
+paranoid = Paranoid()
+paranoid.redirect_view = 'users.login'
 
 mail = Mail()
+migrate = Migrate()
+manager = Manager()
+manager.add_command('db', MigrateCommand)
+
 
 
 def create_app(config_class=Config):
@@ -29,18 +38,18 @@ def create_app(config_class=Config):
     login_manager.init_app(app)
     mail.init_app(app)
     socketio.init_app(app)
+    migrate.init_app(app, db)
+    manager.__init__(app)
+    paranoid.init_app(app)
 
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
     from flaskblog.errors.handlers import errors
 
-
-
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
     app.register_blueprint(errors)
-
 
     return app
--- flaskblog/config.py
+++ flaskblog/config.py
@@ -7,8 +7,16 @@ MAX_SEARCH_RESULTS = 50
 
 
 class Config:
+    ENV = 'dev'
+
+    if ENV == 'dev':
+        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    else:
+
+        SQLALCHEMY_DATABASE_URI = 'postgres://bzzbbjdumbntco:4ee5a8c0e94e274c2c1979b20ac2e93c4395204909169803627e12d7577fbdae@ec2-174-129-255-37.compute-1.amazonaws.com:5432/dcjhrsk5ihpbfr'
+
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
