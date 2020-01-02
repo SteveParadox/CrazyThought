af100c8cf8a4897a9fbfe98cf6680e3888449afd
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-01-02 21:57:02 +0100

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -20,8 +20,8 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = 'fordstphn@gmail.com'
+    MAIL_PASSWORD = 'drstrange'
     SQLALCHEMY_TRACK_MODIFICATIONS= True
     BABEL_DEFAULT_LOCALE = 'en'
 
