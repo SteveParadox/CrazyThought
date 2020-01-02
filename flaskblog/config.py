7a540c6d63060fc426f973634d4b724d89154c67
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-01-02 22:01:59 +0100

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -7,21 +7,12 @@ MAX_SEARCH_RESULTS = 50
 
 
 class Config:
-    ENV = 'prod'
-
-    if ENV == 'dev':
-        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    else:
-
-        SQLALCHEMY_DATABASE_URI = 'postgres://xtxivwdedrdqiq:dca0e349351bad9191732318455e178bd99eec6326c351c09a4d70b475833b32@ec2-174-129-32-240.compute-1.amazonaws.com:5432/dagkfphn1hgj9d'
-
-    SECRET_KEY=os.environ.get('SECRET_KEY')
 
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'fordstphn@gmail.com'
-    MAIL_PASSWORD = 'drstrange'
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
     SQLALCHEMY_TRACK_MODIFICATIONS= True
     BABEL_DEFAULT_LOCALE = 'en'
 
