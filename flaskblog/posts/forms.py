2c3196ba4dd663a55f12900d29a1aab331472778
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-02-09 14:03:46 +0100

    Add files via upload

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -7,7 +7,7 @@ from flask_wtf.file import FileAllowed
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
+    content = TextAreaField('Content',  validators=[DataRequired()])
     photo = FileField('Upload Picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
 
     submit = SubmitField('Post')
