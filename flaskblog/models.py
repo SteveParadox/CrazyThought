92fb8174fb7c5e511b5c80aed9cb2bfe8a6b4c9b
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-04-09 21:02:20 +0100

    Add files via upload

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flaskblog import db, login_manager, app
 from flask_login import UserMixin
@@ -10,6 +11,7 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -35,20 +37,23 @@ class User(db.Model, UserMixin):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
+
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now)
     content = db.Column(db.Text, nullable=False)
     comments = db.Column(db.Integer, default=0)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     img_filename = db.Column(db.String())
     img_data = db.Column(db.LargeBinary)
     comment = db.relationship('Comment', backref='parser', lazy=True)
-    #translate = db.relationship('Translate', backref='translation', lazy=True)
+
+    # translate = db.relationship('Translate', backref='translation', lazy=True)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}'), '{self.content}' "
+        return f"Post( '{self.date_posted}'), '{self.content}' "
 
 
 class Comment(db.Model):
@@ -58,7 +63,7 @@ class Comment(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     name = db.Column(db.String())
     post = db.relationship('Post', backref=db.backref('post', lazy=True))
-    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
     status = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
