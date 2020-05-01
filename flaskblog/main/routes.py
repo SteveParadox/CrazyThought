b565dffd9c55448000399b2b9262ce4f3d7c6a74
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-05-01 11:46:11 +0100

    Add files via upload

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,21 +1,21 @@
 import time
+import random
 
 from flask import Blueprint, jsonify, url_for, redirect, json, flash
 from flask_login import login_required, current_user
 
+
 from flaskblog import db, abort
-from flaskblog.main.form import Searchform, SharePostForm
+from flaskblog.main.form import Searchform, SharePostForm, GetPostForm
 from flaskblog.models import Post, User, Comment
 # from google.cloud import translate_v2 as translate
 from flask import render_template, request
-
 
 from flaskblog.posts.forms import PostForm
 
 '''from pusher import Pusher, pusher.
 import uuid
 '''
-
 
 main = Blueprint('main', __name__)
 
@@ -34,20 +34,47 @@ pusher_client = pusher.Pusher(
 def loader():
     return render_template('loader.html')
 
+''' form2 = GetPostForm()
+form2.content.data = post.content'''
+@main.route("/post/share/<int:post_id>", methods=['GET', 'POST'])
+@login_required
+def share_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    form = SharePostForm()
 
-@main.route('/home', methods= ['GET','POST'])
+
+
+    if form.is_submitted():
+        poss = Post(content=form.content.data, author=current_user)
+
+
+        db.session.add(poss)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('main.home', post_id=post.id))
+    return render_template('share.html', title='Share Post', form=form,
+                           legend='Share Post', post_id=post.id, post=post)
+
+
+
+
+
+@main.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
-
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=8)
-    side = (Post.query.order_by(Post.comments.desc()).all()[0:10])
-    gra= Comment.query.order_by(Comment.pub_date.desc()).all
-    gray = Post.query.filter_by(author=current_user) \
-        .order_by(Post.date_posted.desc())
-    form = PostForm()
-    if form.validate_on_submit():
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=30)
 
+   #ejd= share_post(23)
+
+
+    side = (Post.query.order_by(Post.comments.desc()).all()[0:10])
+    gra = Comment.query.order_by(Comment.pub_date.desc()).all()
+    gry = (Post.query.order_by(Post.content).all())
+    form = PostForm()
+    gray = random.choice(gry)
+
+    if form.validate_on_submit():
         post = Post(content=form.content.data, author=current_user)
         '''translate_client = translate.Client()
 
@@ -66,7 +93,15 @@ def home():
         flash('Your post has been created!', 'success')
         return redirect(url_for('main.home'))
 
-    return render_template('home.html', posts=posts, side=side,  reload = time.time(), gray=gray, form=form)
+    return render_template('home.html', posts=posts, side=side, reload=time.time(), gray=gray, form=form)
+
+
+@main.route('/search', methods=['GET', 'POST'])
+def search():
+    form = Searchform()
+
+
+    return render_template('search.html', form=form)
 
 
 '''
@@ -92,29 +127,3 @@ def search_results(query):
     return render_template('search_results.html', query=query, results=results)
 
 '''
-@main.route("/post/<int:post_id>/share", methods=['GET', 'POST'])
-@login_required
-def share_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    form=SharePostForm()
-    if form.is_submitted():
-        poss= Post( content=form.content.data, author=current_user)
-        posf = Post.query.get_or_404(post_id)
-        db.session.add(poss)
-        db.session.add(posf)
-        db.session.commit()
-        flash('Your post has been created!', 'success')
-        return redirect(url_for('main.home', post_id=post.id))
-    return render_template('share.html', title='Share Post',form=form,
-                            legend='Share Post', post_id=post.id, post=post)
-
-
-
-
-@main.route('/search', methods=['GET', 'POST'])
-def search():
-    searchbox= request.form.get("text")
-   # query = "select word_eng from words where word_eng LIKE '{}%' order by word_eng".format(searchbox)
-    result= User.query.filter_by(username=searchbox).all()
-
-    return jsonify(result)
