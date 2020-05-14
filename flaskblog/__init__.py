90efe5ab06309c73cb843c2eb2b4487299a203c2
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-05-14 12:03:51 +0100

    Add files via upload

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -16,7 +16,7 @@ ma = Marshmallow()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'info'
+login_manager.login_message = None
 socketio = SocketIO()
 
 
