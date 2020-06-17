b8ec73dbf022c243610b176460bc0e91a2e795c3
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-06-17 19:00:44 +0100

    Add files via upload

--- flaskblog/main/form.py
+++ flaskblog/main/form.py
@@ -1,26 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-
-
-from flask_wtf import FlaskForm
-from wtforms import StringField,  SubmitField, TextAreaField, FileField
-from wtforms.validators import DataRequired
 from flask_wtf.file import FileAllowed
+from wtforms import StringField, SubmitField, TextAreaField, FileField
+from wtforms.validators import DataRequired
+
 
 class Searchform(FlaskForm):
-    livebox= StringField(DataRequired())
-    submit= SubmitField('Search')
+    livebox = StringField(DataRequired())
+    submit = SubmitField('Search')
 
 
 class PostForm(FlaskForm):
-    content = TextAreaField( validators=[DataRequired()])
+    content = TextAreaField(validators=[DataRequired()])
     photo = FileField('Upload Picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
 
     submit = SubmitField('Post')
-
 
 
 class SharePostForm(FlaskForm):
@@ -28,5 +21,6 @@ class SharePostForm(FlaskForm):
 
     submit = SubmitField('Share Post')
 
+
 class GetPostForm(FlaskForm):
-    content = TextAreaField( validators=[DataRequired()])
+    content = TextAreaField(validators=[DataRequired()])
--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,20 +1,16 @@
 import time
-import random
 from operator import itemgetter
 
-from flask import Blueprint, jsonify, url_for, redirect, json, flash
-from flask_login import login_required, current_user
-
-
-from flaskblog import db, abort
-from flaskblog.main.form import Searchform, SharePostForm, GetPostForm
-from flaskblog.models import Post, User, Comment, PostSchema, UserSchema
+import requests
+from flask import Blueprint, jsonify, url_for, redirect
 # from google.cloud import translate_v2 as translate
 from flask import render_template, request
+from flask_login import login_required, current_user
 
+from flaskblog import db
+from flaskblog.main.form import Searchform, SharePostForm
+from flaskblog.models import Post, User, UserSchema, Business
 from flaskblog.posts.forms import PostForm
-from flask_marshmallow import Marshmallow
-
 from flaskblog.users.decorator import check_confirmed
 
 '''from pusher import Pusher, pusher.
@@ -38,19 +34,19 @@ pusher_client = pusher.Pusher(
 def loader():
     return render_template('loader.html')
 
+
 ''' form2 = GetPostForm()
 form2.content.data = post.content'''
+
+
 @main.route("/post/share/<int:post_id>", methods=['GET', 'POST'])
 @login_required
 def share_post(post_id):
     post = Post.query.get_or_404(post_id)
     form = SharePostForm()
 
-
-
     if form.is_submitted():
         poss = Post(content=form.content.data, author=current_user)
-
 
         db.session.add(poss)
         db.session.commit()
@@ -60,9 +56,6 @@ def share_post(post_id):
                            legend='Share Post', post_id=post.id, post=post)
 
 
-
-
-
 @main.route('/home', methods=['GET', 'POST'])
 @login_required
 @check_confirmed
@@ -70,11 +63,9 @@ def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=50)
 
+    # ejd= share_post(23)
 
-   #ejd= share_post(23)
-
-
-    
+    side = (Business.query.filter_by().order_by(Business.user_id.desc()).all()[0:5])
     post = Post.query.order_by(Post.content.desc()).all()
     gry = (Post.query.order_by(Post.content).all())
     form = PostForm()
@@ -98,14 +89,11 @@ def home():
 
         return redirect(url_for('main.home'))
 
-    return render_template('home.html', posts=posts, reload=time.time(),  form=form , post=post)
+    return render_template('home.html', posts=posts, side=side, reload=time.time(), form=form, post=post)
 
 
 @main.route('/about', methods=['GET', 'POST'])
 def about():
-
-
-
     return render_template('about.html')
 
 
@@ -117,30 +105,39 @@ def feed():
 '''
 
 
-
 @main.route('/tos')
 def tos():
     return render_template('tos.html')
-
 
 
 @main.route('/cookies')
 def cookies():
     return render_template('cookies.html')
 
+
 @main.route('/search', methods=['GET', 'POST'])
 def search():
-    searchbox =request.form.get("livebox")
-   # db.session.query(User).filter(User.username.data == 1).all()
-    results=  User.query.all()
+    searchbox = request.form.get("livebox")
+    # db.session.query(User).filter(User.username.data == 1).all()
+    results = User.query.all()
     user_schema = UserSchema(many=True)
     res = user_schema.dump(results)
-    result = list(map(itemgetter('id'), res))
+    result = list(map(itemgetter('email', 'username'), res))
     if 'Stephen Ford' in result:
         print('found the name Stephen Ford')
     else:
         print('No Match')
-    return jsonify({'user': result})
+    ip_request = requests.get('https://get.geojs.io/v1/ip.json')
+    my_ip = ip_request.json()['ip']
+    print(my_ip)
+
+    geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
+    geo_request = requests.get(geo_request_url)
+    geo_datas = geo_request.json()
+    geo_data = geo_datas['country']
+    return jsonify({'user': result}, geo_data)
+
+
 '''   x = result["username"]
     x = thisdict.get("model")'''
 
@@ -155,11 +152,11 @@ def searchfile():
     result = list(map(itemgetter('username'), res))
     paxx = form.livebox.data
 
+
     if paxx in result:
         return render_template('search.html', form=form, paxx=paxx, result=result, author=paxx)
     else:
         return render_template('search.html', form=form)
-
 
 
 '''
@@ -172,37 +169,34 @@ def search_results(query):
     return render_template('search_results.html', query=query, results=results)
 
 '''
+
+
 @main.route('/max')
 def index():
     return jsonify("Pong!")
 
-Languages=[
-{
-    'python':'1st rated',
-    'javascript': 'i hate but 2nd rated',
-    'c++': 'my ex crush but 3rd rated',
-    'java': 'just there as my 4th rated',
 
+Languages = [
+    {
+        'python': '1st rated',
+        'javascript': 'i hate but 2nd rated',
+        'c++': 'my ex crush but 3rd rated',
+        'java': 'just there as my 4th rated',
 
-},
-{
-    'vue.js':'1st rated',
-    'jquery': 'i hate but 2nd rated',
-    'react.js': 'my ex crush but 3rd rated',
-    'vanilla.js': 'just there as my 4th rated',
+    },
+    {
+        'vue.js': '1st rated',
+        'jquery': 'i hate but 2nd rated',
+        'react.js': 'my ex crush but 3rd rated',
+        'vanilla.js': 'just there as my 4th rated',
 
-
-}
-    ]
-
+    }
+]
 
 
 @main.route('/total')
 def total():
     return render_template('total.html')
-
-
-
 
 
 @main.route('/home/list', methods=['GET'])
