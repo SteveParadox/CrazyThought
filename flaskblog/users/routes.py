4364134d08cd9f3b9c21461b266ee9c3a082cbd5
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-06-18 17:05:23 +0100

    Add files via upload

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -72,7 +72,7 @@ def register():
 
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password, confirmed=False)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password, confirmed=True)
         db.session.add(user)
         db.session.commit()
         token = generate_confirmation_token(user.email)
@@ -171,18 +171,11 @@ def account():
         .order_by(Post.date_posted.desc()) \
         .paginate(page=page, per_page=10)
 
-   
-    ip_request = requests.get('https://get.geojs.io/v1/ip.json')
-    my_ip = ip_request.json()['ip']
-    print(my_ip)
+    cont = Business.query.filter_by(therapy=current_user).first()
 
-    geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
-    geo_request = requests.get(geo_request_url)
-    geo_datas = geo_request.json()
-    geo_data = geo_datas['country']
 
     return render_template('account.html', title='Account',
-                           image_file=image_file, form=form, posts=posts, geo_data=geo_data)
+                           image_file=image_file, form=form, posts=posts, cont=cont)
 
 
 @users.route('/users/<string:username>')
