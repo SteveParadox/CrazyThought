5c4a24166fabd811939cdac07a196e685553763f
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-07-07 03:05:52 +0100

    Add files via upload

--- flaskblog/posts/routes.py
+++ flaskblog/posts/routes.py
@@ -1,13 +1,10 @@
 import random
-
 from flask import render_template, redirect, url_for, flash, request, abort, Blueprint
 from flask_login import current_user, login_required
-
 from flaskblog import db
-from flaskblog.models import Post, Comment
-from flaskblog.posts.forms import PostForm, UpdatePostForm, CommentForm, ReplyForm
+from flaskblog.models import Post, Comment, Images, Videos, Media_Comment, Media_Comments
+from flaskblog.posts.forms import PostForm, UpdatePostForm, CommentForm, ReplyForm, CommentsForm
 from flaskblog.posts.utils import save_img
-# from google.cloud import translate_v2 as translate
 from flaskblog.users.decorator import check_confirmed
 
 posts = Blueprint('posts', __name__)
@@ -22,6 +19,30 @@ def explore():
     return render_template('explore.html', side=side, title='Popular')
 
 
+@posts.route("/explore/images", methods=['GET', 'POST'])
+@login_required
+@check_confirmed
+def trending():
+    side = (Images.query.order_by(Images.comments.desc()).all()[0:100])
+    x = random.shuffle(side)
+    return render_template('trending.html', side=side, title='Popular')
+
+@posts.route("/explore/videos", methods=['GET', 'POST'])
+@login_required
+@check_confirmed
+def trending_videos():
+    side = (Videos.query.order_by(Videos.comments.desc()).all()[0:100])
+    x = random.shuffle(side)
+    return render_template('trending_videos.html', side=side, title='Popular')
+
+
+@posts.route("/tags", methods=['GET', 'POST'])
+def tags():
+    posts = Post.query.all()
+
+    return render_template('tags.html', posts=posts)
+
+
 @posts.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
@@ -31,18 +52,6 @@ def new_post():
         pic_file = save_img(form.photo.data)
         post = Post(content=form.content.data, author=current_user, img_data=file.read(),
                     img_filename=pic_file)
-        '''translate_client = translate.Client()
-
-        text = form.content.data
-        target = 'es'
-
-        translation = translate_client.translate(
-            text,
-            target_language=target)
-
-        print('Text:  ', text)
-
-        print(u'Translation: {}'.format(translation['translatedText']))'''
         db.session.add(post)
         db.session.commit()
 
@@ -69,7 +78,7 @@ def post(post_id):
         post.comments = post.comments + 1
         db.session.commit()
         return redirect(request.url)
-    return render_template('post.html', post=post, posts=posts, comments=comments, form=form)
+    return render_template('post.html', post=post, posts=posts, comments=comments, form=form, title='Posts')
 
 
 @posts.route("/post/<int:post_id>/comment/<int:comment_id>", methods=['POST'])
@@ -88,39 +97,18 @@ def reply_comment(post_id, comment_id):
     return redirect(url_for(request.url, post_id=post.id, commenta=commenta, form2=form2))
 
 
-"""
-
-    '''translate_client = translate.Client()
-
-    text = Post.query.filter_by(id=post.content)
-    target = 'fr'
-
-    translation = translate_client.translate(
-        text,
-        target_language=target)
-
-    print('Text:  ', text)
-
-    print(u'Translation: {}'.format(translation['translatedText']))'''
-@posts.route("/post/create-entry", methods=['GET', 'POST'])
-@login_required
-def create_entry():
-    req = request.get_json()
-
-    print(req)
-
-    res = make_response(jsonify(req), 200)
-
-    return res
-"""
-
-
 @posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 @check_confirmed
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
     comment = Comment.query.filter_by(post_id=post_id).all()
+    posk = Post.query \
+        .order_by(Post.date_posted.desc()) \
+        .paginate()
+    pots = Post.query.filter_by(author=current_user) \
+        .order_by(Post.date_posted.desc()) \
+        .paginate()
     if post.author != current_user:
         abort(403)
     for o in comment:
@@ -137,7 +125,7 @@ def update_post(post_id):
 
         form.content.data = post.content
     return render_template('create_post.html', title='Update Post',
-                           form=form, legend='Update Post')
+                           form=form, legend='Update Post', posk=posk, pots=pots)
 
 
 @posts.route("/post/<int:post_id>/delete", methods=['POST'])
@@ -177,13 +165,129 @@ def delete_comment(post_id, comment_id):
 @check_confirmed
 def following():
     posts = Comment.query.filter_by(reply=current_user).order_by(Comment.pub_date.desc()).all()
-
     return render_template('following.html', posts=posts, title='My Activities')
+
+@posts.route("/following/images", methods=['GET', 'POST'])
+@login_required
+@check_confirmed
+def image_following():
+    posts = Media_Comments.query.filter_by(reple=current_user).order_by(Media_Comments.pub_date.desc()).all()
+
+    return render_template('image_following.html', posts=posts, title='My Activities')
+
+
+
+@posts.route("/following/videos", methods=['GET', 'POST'])
+@login_required
+@check_confirmed
+def video_following():
+    posts = Media_Comment.query.filter_by(repli=current_user).order_by(Media_Comment.pub_date.desc()).all()
+
+    return render_template('video_following.html', posts=posts, title='My Activities')
 
 
 @posts.route('/report/<int:post_id>')
-@login_required
-@check_confirmed
 def report(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('report.html', post_id=post.id)
+
+
+@posts.route("/post/images/<int:images_id>", methods=['POST', 'GET'])
+@login_required
+@check_confirmed
+def img_post(images_id):
+    page = request.args.get('page', 1, type=int)
+    images=Images.query.get_or_404(images_id)
+    form = CommentsForm()
+    comments = Media_Comments.query.filter_by(images_id=images.id).order_by(Media_Comments.pub_date.desc()).paginate(page=page,
+                                                                                                   per_page=20)
+    if form.validate_on_submit():
+        message = request.form.get('message')
+        comment = Media_Comments(message=message, images_id=images.id, reple=current_user)
+        db.session.add(comment)
+        images.comments = images.comments + 1
+        db.session.commit()
+        return redirect(request.url)
+    return render_template('image_post.html', images=images, form=form, comments=comments)
+
+@posts.route("/post/images/<int:images_id>/delete", methods=['POST'])
+@login_required
+@check_confirmed
+def delete_images(images_id):
+    images=Images.query.get_or_404(images_id)
+    comment = Media_Comments.query.filter_by(images_id=images.id).all()
+    if images.imgs != current_user:
+        abort(403)
+
+    for o in comment:
+        db.session.delete(o)
+    db.session.delete(images)
+
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('main.home'))
+
+
+@posts.route("/post/images/<int:images_id>/comment/<int:comment_id>/delete", methods=['POST'])
+@login_required
+@check_confirmed
+def delete_images_comment(images_id, comment_id):
+    images=Images.query.get_or_404(images_id)
+    comment = Media_Comments.query.get_or_404(comment_id)
+    if comment.reple != current_user:
+        abort(403)
+    db.session.delete(comment)
+    images.comments = images.comments - 1
+    db.session.commit()
+    return redirect(url_for('posts.img_post', images_id=images.id))
+
+
+@posts.route("/post/videos/<int:videos_id>", methods=['POST', 'GET'])
+@login_required
+@check_confirmed
+def vid_post(videos_id):
+    page = request.args.get('page', 1, type=int)
+    videos=Videos.query.get_or_404(videos_id)
+    form = CommentsForm()
+    comments = Media_Comment.query.filter_by(videos_id=videos.id).order_by(Media_Comment.pub_date.desc()).paginate(page=page,
+                                                                                                   per_page=20)
+    if form.validate_on_submit():
+        message = request.form.get('message')
+        comment = Media_Comment(message=message, videos_id=videos.id, repli=current_user)
+        db.session.add(comment)
+        videos.comments = videos.comments + 1
+        db.session.commit()
+        return redirect(request.url)
+    return render_template('video_post.html', videos=videos, form=form, comments=comments)
+
+
+
+@posts.route("/post/videos/<int:videos_id>/delete", methods=['POST'])
+@login_required
+@check_confirmed
+def delete_videos(videos_id):
+    videos=Videos.query.get_or_404(videos_id)
+    comment = Media_Comment.query.filter_by(videos_id=videos.id).all()
+    if videos.vids != current_user:
+        abort(403)
+
+    for o in comment:
+        db.session.delete(o)
+    db.session.delete(videos)
+
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('main.home'))
+
+@posts.route("/post/videos/<int:videos_id>/comment/<int:comment_id>/delete", methods=['POST'])
+@login_required
+@check_confirmed
+def delete_videos_comment(videos_id, comment_id):
+    videos=Videos.query.get_or_404(videos_id)
+    comment = Media_Comment.query.get_or_404(comment_id)
+    if comment.repli != current_user:
+        abort(403)
+    db.session.delete(comment)
+    videos.comments = videos.comments - 1
+    db.session.commit()
+    return redirect(url_for('posts.vid_post', videos_id=videos.id))
