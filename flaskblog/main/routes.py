f2d74702b9ad7a024958e02c3d6c86d244221b3e
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-07-07 03:05:18 +0100

    Add files via upload

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,52 +1,29 @@
 import time
 from operator import itemgetter
-
-import requests
-from flask import Blueprint, jsonify, url_for, redirect
-# from google.cloud import translate_v2 as translate
+from flask import Blueprint, url_for, redirect
 from flask import render_template, request
 from flask_login import login_required, current_user
-
 from flaskblog import db
-from flaskblog.main.form import Searchform, SharePostForm
-from flaskblog.models import Post, User, UserSchema, Business
+from flaskblog.main.form import Searchform, SharePostForm, PhotoForm, VideoForm
+from flaskblog.models import Post, User, UserSchema, Business, Images, Videos
 from flaskblog.posts.forms import PostForm
+from flaskblog.posts.utils import save_img
+from flaskblog.main.utils import save_img as svimg
 from flaskblog.users.decorator import check_confirmed
 
-'''from pusher import Pusher, pusher.
-import uuid
-'''
-
 main = Blueprint('main', __name__)
-
-'''
-pusher_client = pusher.Pusher(
-  app_id='926605',
-  key='1a1e24764be2a75fa3eb',
-  secret='eb8037169d3d98297964',
-  cluster='eu',
-  ssl=True
-)
-'''
-
 
 @main.route('/')
 def loader():
     return render_template('loader.html')
 
-
-''' form2 = GetPostForm()
-form2.content.data = post.content'''
-
-
 @main.route("/post/share/<int:post_id>", methods=['GET', 'POST'])
 @login_required
+@check_confirmed
 def share_post(post_id):
     post = Post.query.get_or_404(post_id)
     form = SharePostForm()
     das= post.content
-
-
     if form.is_submitted():
         poss = Post(content=form.content.data, author=current_user)
         db.session.add(poss)
@@ -63,50 +40,80 @@ def share_post(post_id):
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=50)
-
-    # ejd= share_post(23)
-
     pots = Post.query.filter_by(author=current_user) \
         .order_by(Post.date_posted.desc()) \
         .paginate()
-   
+    posk = Post.query\
+        .order_by(Post.date_posted.desc()) \
+        .paginate()
+    side = (Business.query.filter_by().order_by(Business.user_id.desc()).all()[0:5])
     post = Post.query.order_by(Post.content.desc()).all()
     gry = (Post.query.order_by(Post.content).all())
     form = PostForm()
 
     if form.validate_on_submit():
         post = Post(content=form.content.data, author=current_user)
-        '''translate_client = translate.Client()
-
-        text = form.content.data
-        target = 'es'
-
-        translation = translate_client.translate(
-            text,
-            target_language=target)
-
-        print('Text:  ', text)
-
-        print(u'Translation: {}'.format(translation['translatedText']))'''
         db.session.add(post)
         db.session.commit()
 
         return redirect(url_for('main.home'))
+    form2 = PhotoForm()
 
-    return render_template('home.html', posts=posts, pots=pots,  reload=time.time(), form=form, post=post)
+    if form2.validate_on_submit():
+        file = request.files['photo']
+        photo_file = save_img(form2.photo.data)
+        post = Images(photo_filename=photo_file, photo_data=file.read(), imgs=current_user)
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect(url_for('main.home_images'))
+    form3 = VideoForm()
+
+    if form3.validate_on_submit():
+        file = request.files['video']
+        pic_file = svimg(form3.video.data)
+        post = Videos(img_filename=pic_file, img_data=file.read(), vids=current_user)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('main.home_videos'))
+
+    return render_template('home.html', posts=posts, pots=pots,form2=form2,form3=form3, posk=posk,side=side, reload=time.time(), form=form, post=post)
+
+
+@main.route('/home/images', methods=['GET', 'POST'])
+@login_required
+@check_confirmed
+def home_images():
+    page = request.args.get('page', 1, type=int)
+    posts = Images.query.filter_by(photo_filename=Images.photo_filename).order_by(Images.date_posted.desc()).paginate(page=page, per_page=20)
+    pots = Images.query.filter_by(imgs=current_user) \
+        .order_by(Images.date_posted.desc()) \
+        .paginate()
+    posk = Images.query \
+        .order_by(Images.date_posted.desc()) \
+        .paginate()
+    return render_template('images.html', posts=posts, reload=time.time(), pots=pots, posk=posk)
+
+@main.route('/home/videos', methods=['GET', 'POST'])
+@login_required
+@check_confirmed
+def home_videos():
+    page = request.args.get('page', 1, type=int)
+    posts = Videos.query.filter_by(img_filename=Videos.img_filename).order_by(Videos.date_posted.desc()).paginate(
+        page=page, per_page=20)
+    pots = Videos.query.filter_by(vids=current_user) \
+        .order_by(Videos.date_posted.desc()) \
+        .paginate()
+    posk = Videos.query \
+        .order_by(Videos.date_posted.desc()) \
+        .paginate()
+    return render_template('videos.html', reload=time.time(), posts=posts, pots=pots, posk=posk)
+
 
 
 @main.route('/about', methods=['GET', 'POST'])
 def about():
     return render_template('about.html')
-
-
-'''
-@main.route('/feed')
-def feed():
-    return render_template('feed.html')
-    
-'''
 
 
 @main.route('/tos')
@@ -118,35 +125,8 @@ def tos():
 def cookies():
     return render_template('cookies.html')
 
-
-@main.route('/search', methods=['GET', 'POST'])
-def search():
-    searchbox = request.form.get("livebox")
-    # db.session.query(User).filter(User.username.data == 1).all()
-    results = User.query.all()
-    user_schema = UserSchema(many=True)
-    res = user_schema.dump(results)
-    result = list(map(itemgetter('email', 'username'), res))
-    if 'Stephen Ford' in result:
-        print('found the name Stephen Ford')
-    else:
-        print('No Match')
-    ip_request = requests.get('https://get.geojs.io/v1/ip.json')
-    my_ip = ip_request.json()['ip']
-    print(my_ip)
-
-    geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
-    geo_request = requests.get(geo_request_url)
-    geo_datas = geo_request.json()
-    geo_data = geo_datas['country']
-    return jsonify({'user': result}, geo_data)
-
-
-'''   x = result["username"]
-    x = thisdict.get("model")'''
-
-
 @main.route('/searchfile', methods=['GET', 'POST'])
+@login_required
 @check_confirmed
 def searchfile():
     form = Searchform()
@@ -158,54 +138,11 @@ def searchfile():
     pots = Post.query.filter_by(author=current_user) \
         .order_by(Post.date_posted.desc()) \
         .paginate()
-
-
+    posk = Post.query \
+        .order_by(Post.date_posted.desc()) \
+        .paginate()
     if paxx in result:
-        return render_template('search.html', form=form, paxx=paxx,pots=pots, result=result, author=paxx)
+        return render_template('search.html', form=form, paxx=paxx,pots=pots, posk=posk,result=result, author=paxx)
     else:
-        return render_template('search.html', form=form, pots=pots)
+        return render_template('search.html', form=form, pots=pots, posk=posk)
 
-
-'''
-
-
-@main.route('/search_results/<query>')
-@login_required
-def search_results(query):
-    results = User.query.whoosh_search(query).all()
-    return render_template('search_results.html', query=query, results=results)
-
-'''
-
-
-@main.route('/max')
-def index():
-    return jsonify("Pong!")
-
-
-Languages = [
-    {
-        'python': '1st rated',
-        'javascript': 'i hate but 2nd rated',
-        'c++': 'my ex crush but 3rd rated',
-        'java': 'just there as my 4th rated',
-
-    },
-    {
-        'vue.js': '1st rated',
-        'jquery': 'i hate but 2nd rated',
-        'react.js': 'my ex crush but 3rd rated',
-        'vanilla.js': 'just there as my 4th rated',
-
-    }
-]
-
-
-@main.route('/total')
-def total():
-    return render_template('total.html')
-
-
-@main.route('/home/list', methods=['GET'])
-def api_list():
-    return jsonify(Languages)
