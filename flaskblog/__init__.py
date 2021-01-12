21bcda40f2eb8dadb9596733bed7983132bac06f
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2021-01-12 18:53:38 +0100

    Add files via upload

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -22,32 +22,34 @@ login_manager.session_protection = "strong"
 REMEMBER_COOKIE_NAME= 'remember_token'
 REMEMBER_COOKIE_DURATION=datetime.timedelta(days=64, seconds=29156, microseconds=10)
 REMEMBER_COOKIE_REFRESH_EACH_REQUEST=False
-socketio = SocketIO()
+io = SocketIO()
 mail = Mail()
 jwt = JWTManager()
 
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
     db.init_app(app)
     bcrypt.init_app(app)
     ma.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    socketio.init_app(app)
+    io.init_app(app)
     jwt.init_app(app)
 
     from flaskblog.users.routes import users
     from flaskblog.groups.routes import groups
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+    from flaskblog.rooms.socket_routes import sock
+    from flaskblog.business.routes import business
     from flaskblog.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(groups)
     app.register_blueprint(posts)
+    app.register_blueprint(sock)
     app.register_blueprint(main)
+    app.register_blueprint(business)
     app.register_blueprint(errors)
 
     return app
