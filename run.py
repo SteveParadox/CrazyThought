dc4f8efa5efb63158292fea9c028bbcaabd7ecb7
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2021-01-18 11:01:21 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
-from flaskblog import create_app
+from flaskblog import create_app, io, db
 
 app = create_app()
+app.app_context().push()
 
-ENV= 'prod'
+ENV = 'dev'
 
 if ENV == 'dev':
     app.debug = True
 else:
     app.debug = False
+    threaded = True
+
 
 if __name__ == '__main__':
-    app.run()
+    io.run(app, debug=True, port=5000)
+    #db.drop_all(app=create_app())
+    #db.create_all(app=create_app())
