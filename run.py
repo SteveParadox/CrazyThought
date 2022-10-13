5950bd523d089366a190b60e27511bc7303ad6a5
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2022-10-13 02:03:20 +0100

    Update run.py

--- run.py
+++ run.py
@@ -13,6 +13,6 @@ else:
 
 
 if __name__ == '__main__':
-    io.run(app, debug=True, port=5000)
-    #db.drop_all(app=create_app())
-    #db.create_all(app=create_app())
+    #io.run(app, debug=True, port=5000)
+    db.drop_all(app=create_app())
+    db.create_all(app=create_app())
