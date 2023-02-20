c706ce1b4d0806419c2932b123be420217f42d19
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2023-02-20 21:50:57 +0100

    Update api.py

--- flaskblog/main/api.py
+++ flaskblog/main/api.py
@@ -21,72 +21,6 @@ def home():
 
     return jsonify(res)
 
-@main.route('/home', methods=['GET', 'POST'])
-def home():
-    global pots
-    pots = Post.query.filter_by() \
-        .order_by(Post.date_posted.desc()) \
-        .paginate()
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=50)
-    posk = Post.query \
-        .order_by(Post.date_posted.desc()) \
-        .paginate()
-    side = (Groups.query.filter_by().order_by(Groups.comments.desc()).all()[0:5])
-    post = Post.query.order_by(Post.content.desc()).all()
-    form = PostForm()
-    room= ''
-    if current_user.is_authenticated:
-        room = Room.query.filter_by(host=current_user.username).order_by(Room.created.desc()).all()
-    if form.validate_on_submit():
-        post = Post(content=form.content.data, author=current_user, public_id=str(shortuuid.uuid()))
-        db.session.add(post)
-        db.session.commit()
-
-        return redirect(url_for('main.home'))
-    form2 = PhotoForm()
-
-    if form2.validate_on_submit():
-        file = request.files['photo']
-        photo_file = save_img(form2.photo.data)
-        post = Images(photo_filename=photo_file, photo_data=file.read(), imgs=current_user,
-                      plic_id=str(shortuuid.uuid()))
-        db.session.add(post)
-        db.session.commit()
-
-        return redirect(url_for('main.home_images'))
-    form3 = VideoForm()
-
-    if form3.validate_on_submit():
-        file = request.files['video']
-        pic_file = svimg(form3.video.data)
-        post = Videos(img_filename=pic_file, img_data=file.read(), vids=current_user, publ_id=str(shortuuid.uuid()))
-        db.session.add(post)
-        db.session.commit()
-        return redirect(url_for('main.home_videos'))
-
-    form4 = TopicForm()
-    if form4.validate_on_submit():
-        topic = Topic(name=form4.name.data, creator=current_user, pub_id=str(shortuuid.uuid()))
-        db.session.add(topic)
-        db.session.commit()
-        return redirect(url_for('groups.topics'))
-
-    if current_user.is_authenticated:
-        pots = Post.query.filter_by(author=current_user) \
-            .order_by(Post.date_posted.desc()) \
-            .paginate()
-    return jsonify({
-        "posts": [post.to_dict() for post in posts.items],
-        "pots": [pot.to_dict() for pot in pots.items],
-        "side": [group.to_dict() for group in side],
-        "form": form.to_dict(),
-        "form2": form2.to_dict(),
-        "form3": form3.to_dict(),
-        "form4": form4.to_dict(),
-        "room": [r.to_dict() for r in room],
-    })
-
 @main.route('/api/posts')
 def posts():
     posts = Post.query.order_by(Post.date_posted.desc()).all()
@@ -134,3 +68,16 @@ def create_topic():
     db.session.add(topic)
     db.session.commit()
     return jsonify({'message': 'Topic created!'}), 201
+
+@main_bp.route("/api/post/share/<int:post_id>", methods=['POST'])
+@login_required
+@check_confirmed
+def share_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    content = request.json.get('content')
+    if not content:
+        return jsonify({'error': 'Content is required'}), 400
+    poss = Post(content=content, author=current_user)
+    db.session.add(poss)
+    db.session.commit()
+    return jsonify({'message': 'Post shared successfully'}), 201
