49c3e608715b501157fa3b35c0f5966876f9fa76
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2023-02-20 22:17:32 +0100

    Update post_api.py

--- flaskblog/posts/post_api.py
+++ flaskblog/posts/post_api.py
@@ -56,4 +56,56 @@ def post(public_id):
     rc = ReplyComment.query.order_by(ReplyComment.pub_date.desc()).all()
     if request.method == 'POST':
         message = request.json.get('message')
-        comment = Comment(message=message, post_id=post.id, reply=current
+        comment = Comment(message=message, post_id=post.id, reply=current)
+                          
+                         
+
+@posts.route("/post/<string:public_id>/comment/<int:comment_id>", methods=['GET','POST'])
+@login_required
+@check_confirmed
+def reply_comment(public_id, comment_id):
+    page = request.args.get('page', 1, type=int)
+    post = Post.query.filter_by(public_id=public_id).first()
+    comment = Comment.query.filter_by(id=comment_id).first()
+    rc = ReplyComment.query.filter_by(comment_id=comment.id).order_by(ReplyComment.pub_date.desc()).paginate(page=page, per_page=20)
+    form = ReplyForm()
+    if form.validate_on_submit():
+        rc = ReplyComment(message=form.message.data, comment=current_user, post_id=post.id, comment_id=comment.id)
+        db.session.add(rc)
+        comment.replys = comment.replys + 1
+        db.session.commit()
+        return jsonify({'message': 'Reply added successfully'}), 201
+    return jsonify({'message': 'Invalid Request'}), 400
+
+@posts.route("/post/<string:public_id>/update", methods=['PUT'])
+@login_required
+@check_confirmed
+def update_post(public_id):
+    post = Post.query.filter_by(public_id=public_id).first()
+    if post.author != current_user:
+        return jsonify({'message': 'Forbidden'}), 403
+    data = request.get_json()
+    if not data:
+        return jsonify({'message': 'Invalid Request'}), 400
+    post.content = data['content']
+    post.love = 0
+    db.session.commit()
+    return jsonify({'message': 'Post updated successfully'}), 200
+
+@posts.route("/post/<int:post_id>/delete", methods=['DELETE'])
+@login_required
+@check_confirmed
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        return jsonify({'message': 'Forbidden'}), 403
+    comment = Comment.query.filter_by(post_id=post_id).all()
+    rc = ReplyComment.query.filter_by(post_id=post.id).all()
+    for i in rc:
+        db.session.delete(i)
+    for o in comment:
+        db.session.delete(o)
+    db.session.delete(post)
+    db.session.commit()
+    return jsonify({'message': 'Post deleted successfully'}), 200
+
